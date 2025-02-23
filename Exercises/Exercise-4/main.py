a062f81cb752a639d96b0f536ebccb5c94772aa2
LyndonFan <lyndon0808@gmail.com>
2025-02-23 15:19:48 +0000

    finish ex 4

--- Exercises/Exercise-4/main.py
+++ Exercises/Exercise-4/main.py
@@ -1,10 +1,24 @@
-import boto3
+import csv
+import json
+from pathlib import Path
 
-
-def main():
-    # your code here
-    pass
+def main(folder: Path, destination_file: Path):
+    rows = []
+    for file in folder.rglob("*.json"):
+        with open(file, "r") as f:
+            rows.append(json.load(f))
+    for row in rows:
+        row["geolocation_type"] = row["geolocation"]["type"]
+        row["geolocation_coordinates_0"] = row["geolocation"]["coordinates"][0]
+        row["geolocation_coordinates_1"] = row["geolocation"]["coordinates"][1]
+        row.pop("geolocation")
+    with open(destination_file, "w+", newline="") as fo:
+        writer = csv.DictWriter(fo, fieldnames=list(rows[0].keys()))
+        writer.writeheader()
+        writer.writerows(rows)
+        
 
 
 if __name__ == "__main__":
-    main()
+    file_parent = Path().parent
+    main(file_parent / "data", file_parent / "result.csv")
